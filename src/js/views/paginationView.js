b99848a7d760fb6a73b981879db39adba950fb70
with0p <with2p@yandex.ru>
2021-02-19 19:11:32 +0300

    Current page number and pagination render refactoring

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -3,51 +3,58 @@ import icons from '../../img/icons.svg';
 
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
+  _numPages;
 
   _generateMarkup() {
-    const numPages = Math.ceil(this._data.results.length / this._data.pageSize);
     // page one and there are more pages
-    if (this._data.page === 1 && numPages > 1) {
-      return this._generateButton('next');
+    this._numPages = Math.ceil(this._data.results.length / this._data.pageSize);
+    if (this._data.page === 1 && this._numPages > 1) {
+      return this._generatePagination({ isPrev: false, isNext: true });
     }
 
     // page 1 and no more
-    if ((this._data.page === numPages) === 1) return '';
+    if (this._data.page === 1 && this._numPages === 1)
+      return this._generatePagination({ isPrev: false, isNext: false });
 
     // last page
-    if (this._data.page === numPages && numPages > 1) {
-      return this._generateButton('prev');
+    if (this._data.page === this._numPages && this._numPages > 1) {
+      return this._generatePagination({ isPrev: true, isNext: false });
     }
 
     // other page
-    if (this._data.page < numPages) {
-      return this._generateButton('prev') + this._generateButton('next');
+    if (this._data.page < this._numPages) {
+      return this._generatePagination({ isPrev: true, isNext: true });
     }
   }
 
-  _generateButton(direction) {
-    if (direction === 'next') {
-      return `      
-          <button class="btn--inline pagination__btn--next" data-goto="${
-            this._data.page + 1
-          }">
-          <span>Page ${this._data.page + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>`;
-    }
-    if (direction === 'prev') {
-      return `      
-        <button class="btn--inline pagination__btn--prev" data-goto="${
-          this._data.page - 1
-        }">
-        <svg class="search__icon">
-          <use href="${icons}#icon-arrow-left"></use>
-        </svg>
-        <span>Page ${this._data.page - 1}</span>
-      </button>`;
-    }
+  _generatePagination(pageStatus) {
+    return `
+    <button
+      class="btn--inline pagination__btn--prev ${
+        !pageStatus.isPrev ? 'hidden' : ''
+      }"
+      data-goto="${this._data.page - 1}"
+    >
+      <span>Page ${this._data.page - 1}</span>
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-left"></use>
+      </svg>
+    </button>
+    <div class="pagination-total__text">
+      ${this._data.page} of ${this._numPages}
+    </div>
+    <button
+      class="btn--inline pagination__btn--next ${
+        !pageStatus.isNext ? 'hidden' : ''
+      }"
+      data-goto="${this._data.page + 1}"
+    >
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+      <span>Page ${this._data.page + 1}</span>
+    </button>
+    `;
   }
 
   addHandlerClick(handler) {
